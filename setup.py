858d769b577074088af7b7a8cdc3cc1402c4482e
Jacob Wiltse <kelwing@kelnet.org>
2018-01-18 06:44:51 +0000

    Update setup.py to include github URL

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://kuvien.io',  # Optional
+    url='https://github.com/Kelwing/pykuvien',  # Optional
 
     # This should be your name or the name of the organization which owns the
     # project.
